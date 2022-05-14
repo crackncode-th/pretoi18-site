94173800344d125a28926f539cbbad11c940efc5
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-14 22:36:53 +0700

    Add index and rules

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
   import "../app.css";
+
+  import { goto } from "$app/navigation";
+
+  const routes = {
+    rules: "Rules",
+    schedule: "Schedule",
+    about: "About",
+  };
 </script>
 
-<slot />
+<nav class="flex flex-row justify-between items-center p-4 w-2/3 mx-auto">
+  <div class="left">
+    <img
+      on:click={() => {
+        goto("/");
+      }}
+      class="cursor-pointer"
+      width="96"
+      src="crackncode.webp"
+      alt="crack n code logo"
+    />
+  </div>
+
+  <div class="right px-4 flex flex-row justify-end items-center gap-2">
+    {#each Object.entries(routes) as [url, name]}
+      <a
+        class="text-xl hover:bg-slate-300 p-2 rounded transition-all"
+        href={url}
+      >
+        {name}
+      </a>
+    {/each}
+  </div>
+</nav>
+
+<main class="p-4">
+  <slot />
+</main>
