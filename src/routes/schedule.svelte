03bfd4a34ff202ef97656760ebccc1a1b6426db6
khajornritdacha <8501582+khajornritdacha@users.noreply.github.com>
2022-05-14 23:56:02 +0700

    Prettify schedule.svelte

--- src/routes/schedule.svelte
+++ src/routes/schedule.svelte
@@ -1,40 +1,4 @@
-
-  <div class="container content-center">
-    <div class="heading">
-      <h2><span>กำหนดการ</span></h2>
-      <p class="heading-p">วันที่และเวลาทั้งหมดจะอยู่ในเวลามาตรฐานประเทศไทย (GMT+7)</p>
-    </div>
-    
-    <table class="main-ta border border-collapse border-white">
-      <thead><tr><th>วันที่</th><th>เวลา</th><th>กิจกรรม</th></tr></thead><tbody
-      >
-        <tr
-          ><td>จันทร์ที่ 16 พฤษภาคม 2565</td><td> 00:00 น.</td><td
-            >เปิดรับสมัครผู้เข้าแข่งขัน</td
-          ></tr
-        ><tr
-          ><td>พฤหัสที่ 26 พฤษภาคม 2565</td><td> 23:59 น.</td><td>ปิดรับสมัครผู้เข้าแข่งขัน</td
-          ></tr
-        ><tr
-          ><td>ศุกร์ที่ 27 พฤษภาคม 2565</td><td
-            > 19:00 น.</td
-          ><td>ผู้เข้าแข่งขันทดสอบระบบ</td></tr
-        ><tr
-          ><td>เสาร์ที่ 28 พฤษภาคม 2565</td><td> 19:00 น. - 22:00 น.</td><td>การแข่งขันวันที่ 1</td
-          ></tr
-        ><tr
-          ><td>อาทิตย์ที่ 29 พฤษภาคม 2565</td><td> 19:00 น. - 22:00 น. </td><td>การแข่งขันวันที่ 2</td
-          ></tr
-        ><tr
-        ><td>จันทร์ที่ 30 พฤษภาคม 2565</td><td> 19.00 น. </td><td>ประกาศผลการแข่งขัน</td
-        ></tr
-      >
-        </tbody
-      >
-    </table>
-  </div>
-
-<style lang="postcss">
+<style lang="postcss"></style>
   .container {
     text-align: center;
     font-size: 1.25rem;
@@ -43,7 +7,7 @@
 
   .heading {
     font-size: 2rem;
-    margin-bottom: 25px; 
+    margin-bottom: 25px;
     padding: 0 50px;
   }
 
@@ -64,6 +28,57 @@
     @apply border;
     @apply ta-ele;
   }
-
-
 </style>
+
+<div class="container content-center">
+  <div class="heading">
+    <h2>
+      <span>กำหนดการ</span>
+    </h2>
+    <p class="heading-p">
+      วันที่และเวลาทั้งหมดจะอยู่ในเวลามาตรฐานประเทศไทย (GMT+7)
+    </p>
+  </div>
+
+  <table class="main-ta border border-collapse border-white">
+    <thead>
+      <tr>
+        <th>วันที่</th>
+        <th>เวลา</th>
+        <th>กิจกรรม</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>จันทร์ที่ 16 พฤษภาคม 2565</td>
+        <td>00:00 น.</td>
+        <td>เปิดรับสมัครผู้เข้าแข่งขัน</td>
+      </tr>
+      <tr>
+        <td>พฤหัสที่ 26 พฤษภาคม 2565</td>
+        <td>23:59 น.</td>
+        <td>ปิดรับสมัครผู้เข้าแข่งขัน</td>
+      </tr>
+      <tr>
+        <td>ศุกร์ที่ 27 พฤษภาคม 2565</td>
+        <td>19:00 น.</td>
+        <td>ผู้เข้าแข่งขันทดสอบระบบ</td>
+      </tr>
+      <tr>
+        <td>เสาร์ที่ 28 พฤษภาคม 2565</td>
+        <td>19:00 น. - 22:00 น.</td>
+        <td>การแข่งขันวันที่ 1</td>
+      </tr>
+      <tr>
+        <td>อาทิตย์ที่ 29 พฤษภาคม 2565</td>
+        <td>19:00 น. - 22:00 น.</td>
+        <td>การแข่งขันวันที่ 2</td>
+      </tr>
+      <tr>
+        <td>จันทร์ที่ 30 พฤษภาคม 2565</td>
+        <td>19.00 น.</td>
+        <td>ประกาศผลการแข่งขัน</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
