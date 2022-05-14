e19ba899453d036297601ce3f10f990423021e2f
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-15 01:56:12 +0700

    Fix Build Error, feat. Git

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@
 
 import adapter from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
+import path from "node:path";
 
 /** @type {import("@sveltejs/kit").Config} */
 export default {
@@ -15,5 +16,12 @@ export default {
 
   kit: {
     adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          $icons: path.resolve("src/icons"),
+        },
+      },
+    },
   },
 };
