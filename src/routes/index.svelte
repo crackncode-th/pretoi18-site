e8205e7a41f495fcfc8e10889c47f03f2cd0cba6
Idhibhat Pankam <marginpankam@gmail.com>
2022-05-15 00:34:28 +0700

    register button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,24 @@
+<script lang="ts">
+  const handleLink = () => {
+    window.open(
+      "https://docs.google.com/forms/d/17Vf0IOAQ9NY8uC472wbZdhx0hFEBMMPiB0IDMxbG_E4/viewform?edit_requested=true"
+    );
+  };
+</script>
+
 <main class="page">
   <h1 class="text-6xl font-bold mb-10">Crack 'n' Code Pre TOI18</h1>
+  <h2 class="mb-20">
+    TOI ใกล้เข้ามาแล้ว อยากจะทดสอบฝีมือตัวเองใช่มั้ย คุณคิดว่าคุณพร้อมแค่ไหนกัน
+    กดปุ่มลงทะเบียนแข่งด้านล่างเลยยย
+  </h2>
+  <button
+    on:click={handleLink}
+    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+    ><h2>ลงทะเบียน</h2></button
+  >
 
-  <h2>เราคือใคร</h2>
-  <p>
-    Crack 'n' Code Thailand เป็นกลุ่มนักเรียนโอลิมปิกวิชาการคอมพิวเตอร์
-    (ค่ายสสวท.)
-    ที่ต้องการสร้างพื้นที่ออนไลน์เพื่อให้นักเรียนที่มีความสนใจด้านการแข่งขันคอมพิวเตอร์โอลิมปิกได้ฝึกฝน
-    และพัฒนาความสามารถของตนเอง
-    ให้มีความสามารถในการแก้ไขปัญหาเชิงคอมพิวเตอร์ได้ดีขึ้น
-  </p>
-
-  <h2>การแข่งขัน Pre-TOI 18</h2>
+  <h2 class="mt-40">การแข่งขัน Pre-TOI 18</h2>
   <p>
     จะจัดขึ้นในวันที่ 28-29 พฤษภาคม 2565 ผ่านระบบ CMS
     ซึ่งเป็นระบบเดียวกับที่ใช้ในการแข่งขันคอมพิวเตอร์โอลิมปิกระหว่างประเทศ (IOI)
