a76c4f698cf4359fdc3034cd7c7adf96aa5aaf9f
Idhibhat Pankam <marginpankam@gmail.com>
2022-05-14 23:34:09 +0700

    about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,58 @@
+<main class="w-2/3 mx-auto">
+  <h1 class="text-center text-6xl font-bold mb-10">เกี่ยวกับเรา</h1>
+
+  <h2>เราคือใคร</h2>
+  <p>
+    Crack 'n' Code Thailand เป็นกลุ่มนักเรียนคอมพิวเตอร์โอลิมปิกวิชาการ
+    (ค่ายสสวท.)
+    ที่ต้องการสร้างพื้นที่ออนไลน์เพื่อให้นักเรียนที่มีความสนใจด้านการแข่งขันคอมพิวเตอร์โอลิมปิกได้ฝึกฝน
+    และพัฒนาความสามารถของตนเอง
+    ให้มีความสามารถในการแก้ไขปัญหาเชิงคอมพิวเตอร์ได้ดีขึ้น
+  </p>
+
+  <h2>หมายเหตุ</h2>
+  <p class="mb-3">
+    พวกเรา<span class="font-bold text-red-600">ไม่ใช่</span
+    >คณะผู้จัดส่งผู้แทนโอลิมปิกวิชาการ
+    และมิได้มีส่วนเกี่ยวข้องกับองค์กรที่เกี่ยวกับโอลิมปิกวิชาการ (เช่น สอวน.
+    หรือ สสวท.) โดยตรง
+  </p>
+  <p>
+    พวกเราเป็นแค่กลุ่มนักเรียนคอมพิวเตอร์โอลิมปิกวิชาการ
+    ที่อยากช่วยพัฒนาความก้าวหน้าวงการคอมพิวเตอร์โอลิมปิกวิชาการในไทย
+  </p>
+
+  <h2 class="mt-20">About Us</h2>
+  <p>
+    Crack 'n' Code Thailand is a group of Thai students who have been through
+    olympic training in informatics. We want to create an online space that
+    hosts computer olympic contests for hungry minds who wants to test their
+    programming abilities, sharpen their problem-solving skills, and unleash
+    their creativity.
+  </p>
+
+  <h2>Disclaimer</h2>
+  <p class="mb-3">
+    We are<span class="font-bold text-red-600">NOT</span> the official organization
+    involved in the selection and training process of Thai students in the fields
+    of informatics. Those processes are officially handled by The Institute for the
+    Promotion of Teaching Science and Technology (IPST) and The Promotion of Academic
+    Olympiad and Development of Science Education Foundation under the patronage
+    of Her Royal Highness Princess Galyani Vadhana Krom Luang Naradhiwas Rajanagarindra
+    (POSN).
+  </p>
+  <p>
+    We are just some people who want to help foster the computer olympic
+    community in Thailand.
+  </p>
+</main>
+
+<style lang="postcss">
+  h2 {
+    @apply text-2xl font-bold;
+  }
+
+  p {
+    @apply text-lg my-4;
+  }
+</style>
