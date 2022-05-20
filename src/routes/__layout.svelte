2915161925ef6a4be1ae10355d50bbe2038f2dd7
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-20 15:13:52 +0700

    Update Nav in Mobile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,8 +7,10 @@
   import Facebook from "$icons/Facebook.svelte";
   import GitHub from "$icons/GitHub.svelte";
   import Instagram from "$icons/Instagram.svelte";
+  import List from "$icons/List.svelte";
 
   import ext from "$lib/ext";
+  import { onMount } from "svelte";
 
   const routes = {
     "/": "Home",
@@ -16,6 +18,12 @@
     schedule: "Schedule",
     about: "About",
   };
+
+  let popup = false;
+
+  onMount(() => {
+    window.addEventListener("click", () => (popup = false));
+  });
 </script>
 
 <nav class="flex flex-row justify-between items-center p-4 rwidth mx-auto">
@@ -31,7 +39,10 @@
     />
   </div>
 
-  <div class="right px-4 flex flex-row justify-end items-center gap-2">
+  <!-- Desktop Nav -->
+  <div
+    class="links px-4 flex-row hidden sm:flex justify-end items-center gap-2"
+  >
     {#each Object.entries(routes) as [url, name]}
       <a
         class="text-xl {(
@@ -41,6 +52,35 @@
         )
           ? 'bg-slate-800'
           : 'hover:bg-slate-700'} p-2 rounded transition-all"
+        href={url}
+      >
+        {name}
+      </a>
+    {/each}
+  </div>
+
+  <!-- Mobile Nav -->
+  <div
+    class="inline sm:hidden bg-slate-800 hover:bg-slate-600 p-2 rounded"
+    on:click={() => setTimeout(() => (popup = !popup), 10)}
+  >
+    <List />
+  </div>
+
+  <div
+    class="bg-slate-800 rounded absolute top-4 p-2 flex flex-col {popup
+      ? 'right-2 opacity-100'
+      : '-right-28 opacity-0'} transition-all"
+  >
+    {#each Object.entries(routes) as [url, name]}
+      <a
+        class="text-xl {(
+          url == '/'
+            ? $page.url.pathname == '/'
+            : $page.url.pathname == '/' + url
+        )
+          ? 'text-pink-500'
+          : 'text-white'} p-2 rounded transition-all"
         href={url}
       >
         {name}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,7 +14,7 @@
     {...ext}
   >
     <button
-      class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+      class="border focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 border-red-500 text-red-500 hover:text-white hover:bg-red-600 focus:ring-red-900 transition-all"
     >
       <h2>ลงทะเบียน</h2>
     </button>
