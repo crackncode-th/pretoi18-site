3944e88fd24754a1beddc594b6d0cfd1443205d3
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-27 00:26:08 +0700

    Remove countdown elements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,3 @@
-<script lang="ts">
-  import Countdown from "$components/Countdown.svelte";
-
-  const pastRegistration =
-    new Date().getTime() >= new Date("2022-05-26 23:59:59").getTime();
-</script>
-
 <main class="page">
   <h1 class="page-title mb-10">Crack 'n' Code Pre TOI18</h1>
   <h2 class="mb-20">
@@ -12,24 +5,12 @@
     กดปุ่มลงทะเบียนแข่งด้านล่างเลยยย
   </h2>
 
-  <div class="buttons flex flex-row justify-center gap-2">
-    <button
-      on:click={() =>
-        window.open(
-          "https://docs.google.com/forms/d/17Vf0IOAQ9NY8uC472wbZdhx0hFEBMMPiB0IDMxbG_E4"
-        )}
-      class:hidden={pastRegistration}
-    >
-      <h2>ลงทะเบียน</h2>
-    </button>
-    <button on:click={() => window.open("http://209.97.175.45/")}>
-      <h2>เข้าระบบเกรดเดอร์</h2>
-    </button>
-  </div>
-
-  <div class:hidden={pastRegistration}>
-    <Countdown />
-  </div>
+  <button
+    class="border rounded-lg px-5 py-2.5 border-red-500 text-red-500 hover:text-white hover:bg-red-600 focus:ring-red-900 transition-all select-none"
+    on:click={() => window.open("http://209.97.175.45/")}
+  >
+    <h2>เข้าระบบเกรดเดอร์</h2>
+  </button>
 
   <h2 class="mt-20">การแข่งขัน Pre-TOI 18</h2>
   <p>
@@ -38,9 +19,3 @@
     ที่จะมาถึงนี้
   </p>
 </main>
-
-<style lang="postcss">
-  .buttons > button {
-    @apply border rounded-lg px-5 py-2.5 border-red-500 text-red-500 hover:text-white hover:bg-red-600 focus:ring-red-900 transition-all select-none;
-  }
-</style>
