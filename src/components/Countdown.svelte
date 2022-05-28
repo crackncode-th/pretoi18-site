0f0c7b3581acd47f1e78c69c777e9ca01e5d1f25
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-05-28 15:53:33 +0700

    Add Countdown for Day 1

---
+++ src/components/Countdown.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  const targetTime = new Date("2022-05-28 19:00:00").getTime();
+  let text = "...";
+  function setTime() {
+    const now = new Date().getTime();
+    const diff = targetTime - now;
+    const totalsec = Math.floor(diff / 1000);
+    const sec = totalsec % 60;
+    const totalmin = Math.floor(totalsec / 60);
+    const min = totalmin % 60;
+    const totalhour = Math.floor(totalmin / 60);
+    text = `${totalhour} ชั่วโมง ${min} นาที ${sec} วินาที`;
+  }
+  onMount(() => {
+    const interval = setInterval(setTime, 500);
+    return () => clearInterval(interval);
+  });
+</script>
+
+<p class="text-4xl font-bold text-red-500">{text}</p>
+<p class="text-red-500 text-xl font-semibold mb-4">(เริ่มเวลา 19:00 น.)</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,23 @@
+<script>
+  import Countdown from "$components/Countdown.svelte";
+</script>
+
 <main class="page">
   <h1 class="page-title mb-10">Crack 'n' Code Pre TOI18</h1>
   <h2 class="mb-20">
     TOI ใกล้เข้ามาแล้ว อยากจะทดสอบฝีมือตัวเองใช่มั้ย คุณคิดว่าคุณพร้อมแค่ไหนกัน
-    กดปุ่มลงทะเบียนแข่งด้านล่างเลยยย
   </h2>
 
-  <p class="text-2xl text-red-500">หมายเหตุ: หมดเขตรับสมัครแล้ว</p>
+  {#if new Date().getTime() <= new Date("2022-05-28 19:00:00").getTime()}
+    <p class="text-3xl font-semibold text-red-500">
+      หมดเขตรับสมัครแล้ว จะเริ่มแข่งขันวันที่ 1 ในอีก
+    </p>
+    <Countdown />
+  {:else}
+    <p class="text-3xl font-semibold text-red-500">
+      หมดเขตรับสมัครแล้ว การแข่งขันวันที่ 1 ได้เริ่มขึ้นแล้ว
+    </p>
+  {/if}
 
   <button
     class="border rounded-lg px-5 py-2.5 border-red-500 text-red-500 hover:text-white hover:bg-red-600 focus:ring-red-900 transition-all select-none"
